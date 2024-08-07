8e9f844672fd17c4ecf36d0cd3422f55413d8128
darknil <jrmlelantus@gmail.com>
2024-08-08 01:58:43 +0200

    fix

--- server/src/rooms/schema/MyRoomState.ts
+++ server/src/rooms/schema/MyRoomState.ts
@@ -1,12 +1,11 @@
 import { Schema, MapSchema, type } from "@colyseus/schema";
 
 export class Player extends Schema {
-  @type('number')
-  x: number = 0;
-
-  @type('number')
-  y: number = 0;
+  @type("number") x: number = 0;
+  @type("number") y: number = 0;
 }
+
 export class MyRoomState extends Schema {
+
   @type({ map: Player }) players = new MapSchema<Player>();
 }
