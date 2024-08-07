2f2205681cddc39ac160b74290619ab23afceded
darknil <jrmlelantus@gmail.com>
2024-08-08 01:43:48 +0200

    fix govnocode

--- server/src/rooms/MyRoom.ts
+++ server/src/rooms/MyRoom.ts
@@ -1,41 +1,25 @@
 import { Room, Client } from "@colyseus/core";
 import { MyRoomState, Player } from "./schema/MyRoomState";
 
-export type PositionMessage = {
-  x: number,
-  y: number
-}
-
 export class MyRoom extends Room<MyRoomState> {
   maxClients = 4;
 
   onCreate (options: any) {
+    console.log("Room created with options:", options);
     this.setState(new MyRoomState());
+    
 
     this.onMessage('move', (client, payload) => {
       //
       // handle "type" message
       //
       const player = this.state.players.get(client.sessionId);
-      const velocity = 2;
-
-      if (payload.left) {
-        player.x -= velocity;
-
-      } else if (payload.right) {
-        player.x += velocity;
-      }
-
-      if (payload.up) {
-        player.y -= velocity;
-
-      } else if (payload.down) {
-        player.y += velocity;
+      if (player) {
+          player.x += payload.x;
+          player.y += payload.y;
       }
       console.log(client.sessionId, "moved", payload);
     });
-
-    this.setSimulationInterval((deltaTime) => this.update(deltaTime));
   }
 
   update(deltaTime: number) {
@@ -46,26 +30,17 @@ export class MyRoom extends Room<MyRoomState> {
   }
   onJoin (client: Client, options: any) {
     console.log(client.sessionId, "joined!");
-
     this.state.players.set(client.sessionId, new Player());
-
+    const player = this.state.players.get(client.sessionId);
     // Send welcome message to the client.
-    client.send("welcomeMessage", "Welcome to Colyseus!");
+    client.send("welcome", {player: player , message: "Welcome to the game!" });
 
-    // Listen to position changes from the client.
-    this.onMessage("position", (client, position: PositionMessage) => {
-      console.log({position})
-      const player = this.state.players.get(client.sessionId);
-      player.x = position.x;
-      player.y = position.y;
-      console.log({position})
-      client.send("welcomeMessage", `position: ${position.x}, ${position.y}`);
-    });
   }
 
   onLeave (client: Client, consented: boolean) {
     console.log(client.sessionId, "left!");
     this.state.players.delete(client.sessionId);
+    this.broadcast("playerLeft", { sessionId: client.sessionId });
   }
 
   onDispose() {
